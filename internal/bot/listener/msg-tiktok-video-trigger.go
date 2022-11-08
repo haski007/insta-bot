16188028c07a2323b8cabab4fe4e7084ca24231c
haski007 <haski007001@gmail.com>
2022-11-08 01:45:57 +0000

    feat: added tik tok video downloading support | forgot to commit new files :)

---
+++ internal/bot/listener/msg-tiktok-video-trigger.go
@@ -0,0 +1,86 @@
+package listener
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/haski007/insta-bot/internal/bot/publisher"
+	"github.com/haski007/insta-bot/pkg/file"
+	"github.com/haski007/insta-bot/pkg/text"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func (rcv *InstaBotService) msgTikTokTrigger(update tgbotapi.Update) {
+	chatID := update.Message.Chat.ID
+	messageID := update.Message.MessageID
+	url := update.Message.Text
+
+	videoData, err := rcv.tiktokApi.GetVideoDataFromUrl(url)
+	if err != nil {
+		rcv.log.WithError(err).Error("[msgTikTokTrigger] send media group")
+		return
+	}
+
+	fileName := videoData.GetAuthor() + strconv.FormatInt(time.Now().Unix(), 10)
+	videoPath, err := videoData.DownloadAsFile(os.TempDir(), fileName)
+	if err != nil {
+		rcv.SendError(chatID, ErrInternalServerError)
+		if errC := rcv.NotifyCreator(fmt.Sprintf("[msgTikTokTrigger] can't download as file url: %s\n"+
+			"from chat with: @%s\n",
+			videoData.GetDownloadUrl(),
+			update.Message.From.UserName)); errC != nil {
+			rcv.log.WithError(err).Println("[msgTikTokTrigger] notify creator")
+		}
+		rcv.log.WithError(err).Error("[msgTikTokTrigger] download as file")
+		return
+	}
+
+	fileBytes, err := getTikTokFileBytes(videoPath, fileName)
+	if err != nil {
+		rcv.SendError(chatID, ErrInternalServerError)
+		if errC := rcv.NotifyCreator(fmt.Sprintf("[msgTikTokTrigger] can't get file bytes videoPath: %s\n"+
+			"from chat with: @%s\n",
+			videoPath,
+			update.Message.From.UserName)); errC != nil {
+			rcv.log.WithError(err).Println("[msgTikTokTrigger] notify creator")
+		}
+		rcv.log.WithError(err).Error("[msgTikTokTrigger] download as file")
+		return
+	}
+
+	var message = fmt.Sprintf("TikTokVideo:\n"+
+		"Author: [%s](%s)\n"+
+		"Source: [tiktok](%s)\n"+
+		"title/caption:\n%s\n\n"+
+		"shared by: @%s %s\n",
+		videoData.GetAuthor(), publisher.TikTokBaseUrl+"@"+videoData.GetAuthor(),
+		url,
+		text.CharLimiterToWord(videoData.GetTitle(), rcv.captionCharsLimit),
+		update.Message.From.UserName, update.Message.From.FirstName+" "+update.Message.From.LastName)
+	videoMessage := tgbotapi.NewVideo(chatID, fileBytes)
+	videoMessage.ParseMode = tgbotapi.ModeMarkdown
+	videoMessage.Caption = message
+	if _, err := rcv.bot.Send(videoMessage); err != nil {
+		rcv.log.WithError(err).Error("[msgTikTokTrigger] send video message")
+		return
+	}
+
+	if err := rcv.DeleteMessage(chatID, messageID); err != nil {
+		rcv.log.WithError(err).Error("[msgTikTokTrigger] delete message")
+	}
+}
+func getTikTokFileBytes(filepath, name string) (tgbotapi.FileBytes, error) {
+	defer file.DeleteFile(filepath)
+	photoBytes, err := os.ReadFile(filepath)
+	if err != nil {
+		return tgbotapi.FileBytes{}, fmt.Errorf("read file err: %w")
+	}
+	photoFileBytes := tgbotapi.FileBytes{
+		Name:  name,
+		Bytes: photoBytes,
+	}
+	return photoFileBytes, nil
+}
