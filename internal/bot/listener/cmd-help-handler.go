c6e529c37ac14b60f588462099f499e899e40b9e
haski007 <haski007001@gmail.com>
2025-04-15 12:23:50 +0200

    fix:  panic when add to a new chat

--- internal/bot/listener/cmd-help-handler.go
+++ internal/bot/listener/cmd-help-handler.go
@@ -13,17 +13,36 @@ func (rcv *InstaBotService) cmdStartHandler(update tgbotapi.Update) {
 }
 
 func (rcv *InstaBotService) sendStartInfo(update tgbotapi.Update) error {
+	if rcv == nil {
+		return fmt.Errorf("InstaBotService is nil")
+	}
+
 	message := `Нащо ви мене знову розбудили ?! 😡`
 
 	var chatID int64
 	var chatTitle string
+
+	// Try to get chat info from MyChatMember first
 	if update.MyChatMember != nil {
 		chatID = update.MyChatMember.Chat.ID
 		chatTitle = update.MyChatMember.Chat.Title
 	} else if update.Message != nil {
+		// Fallback to Message if MyChatMember is not available
 		chatID = update.Message.Chat.ID
 		chatTitle = update.Message.Chat.Title
+	} else {
+		return fmt.Errorf("no valid chat information found in update")
 	}
-	rcv.NotifyCreator(fmt.Sprintf("Bot was added to a new chat %d, chat title: %s", chatID, chatTitle))
+
+	// Only notify creator if we have valid chat information
+	if chatID != 0 {
+		if err := rcv.NotifyCreator(fmt.Sprintf("Bot was added to a new chat %d, chat title: %s", chatID, chatTitle)); err != nil {
+			// Log the error but don't return it since it's not critical
+			if rcv.log != nil {
+				rcv.log.WithError(err).Error("[sendStartInfo] failed to notify creator")
+			}
+		}
+	}
+
 	return rcv.SendMessage(chatID, message)
 }
